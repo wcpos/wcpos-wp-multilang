40bdb01ae9dae11f75ad939c6830a1f7abcdab38
Paul Kilmurray <paul@kilbot.com>
2026-04-21 15:59:43 +0200

    chore: add Update URI header and bump version (#12)
    * chore: add Update URI header
    
    * chore: bump version to 0.1.1

--- wcpos-wp-multilang.php
+++ wcpos-wp-multilang.php
@@ -2,8 +2,9 @@
 /**
  * Plugin Name: WCPOS WP Multilang Integration
  * Description: WP Multilang language filtering for WCPOS, including fast-sync route coverage and per-store language support in WCPOS Pro.
- * Version: 0.1.0
+ * Version: 0.1.1
  * Author: kilbot
+ * Update URI:  https://github.com/wcpos/wcpos-wp-multilang
  * Requires Plugins: woocommerce, wp-multilang
  * Text Domain: wcpos-wp-multilang
  */
@@ -14,7 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-const VERSION = '0.1.0';
+const VERSION = '0.1.1';
 
 require_once __DIR__ . '/includes/class-plugin.php';
 
